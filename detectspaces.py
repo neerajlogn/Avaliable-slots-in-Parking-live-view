ed009704c46c1b0ff03d41f9c7b52fae5dcbc535
neerajlogn <108924306+neerajlogn@users.noreply.github.com>
2022-10-23 00:31:10 +0530

    Create detectspaces.py

---
+++ detectspaces.py
@@ -0,0 +1,36 @@
+import cv2
+import pickle
+
+
+
+img = cv2.imread('fcarpark.jpeg')
+posList = []
+
+width , height = 120,210
+try:
+    with open('CarParkPos', 'rb') as f:
+     posList=pickle.load(f)
+except:
+    posList = []
+
+
+
+def mouseclick(events , x,y,flags , params):
+    if events == cv2.EVENT_LBUTTONDOWN:
+        posList.append((x,y))
+    if events == cv2.EVENT_RBUTTONDOWN:
+        for i,pos in enumerate(posList):
+            x1,y1 = pos
+            if x1<x<x1+width and y1<y<y1+height:
+                posList.pop(i)
+    with open('CarParkPos','wb') as f:
+        pickle.dump(posList,f)
+
+
+while True:
+    img = cv2.imread('fcarpark.jpeg')
+    for pos in posList:
+        cv2.rectangle(img,pos,(pos[0]+width,pos[1]+height),(255,0,255),2)
+    cv2.imshow("Image",img)
+    cv2.setMouseCallback("Image",mouseclick)
+    cv2.waitKey(1)
